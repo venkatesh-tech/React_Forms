49c4af7488ae24561b758e8054f08f053869fbd0
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-23 18:50:54 +0530

    HOC-2

--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
+import UpdatedComponent from "./withCounter";
 
 class HoverCounter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       count: 0,
+  //     };
+  //   }
 
-  incrementCount = () => {
-    this.setState((prevState) => {
-      return { count: prevState.count + 1 };
-    });
-  };
+  //   incrementCount = () => {
+  //     this.setState((prevState) => {
+  //       return { count: prevState.count + 1 };
+  //     });
+  //   };
   render() {
-    const { count } = this.state;
+    const { count, incrementCount } = this.props;
     return (
       <div>
-        <h2 onMouseOver={this.incrementCount}>Hovered {count} times</h2>
+        <h2 onMouseOver={incrementCount}>Hovered {count} times</h2>
       </div>
     );
   }
 }
 
-export default HoverCounter;
+export default UpdatedComponent(HoverCounter);
